c68e822b1d1e6087194d5555adc3e207062ca530
Joel Galenson <jgalenson@google.com>
2021-08-11 15:58:47 +0000

    Revert^2 "Upgrade rust/crates/x509-parser to 0.10.0"
    c5051d11821e3e739e35ce22bc1c7b9a9decb9b4
    
    Change-Id: I1961432f7868bb7f7b9e0afd0c0200ecc53a6bc0

--- examples/print-cert.rs
+++ examples/print-cert.rs
@@ -4,6 +4,12 @@ use std::cmp::min;
 use std::env;
 use std::io;
 use x509_parser::prelude::*;
+#[cfg(feature = "validate")]
+use x509_parser::validate::Validate;
+
+const PARSE_ERRORS_FATAL: bool = false;
+#[cfg(feature = "validate")]
+const VALIDATE_ERRORS_FATAL: bool = false;
 
 fn print_hex_dump(bytes: &[u8], max_len: usize) {
     let m = min(bytes.len(), max_len);
@@ -14,9 +20,23 @@ fn print_hex_dump(bytes: &[u8], max_len: usize) {
 }
 
 fn format_oid(oid: &Oid) -> String {
-    match oid2sn(oid) {
+    match oid2sn(oid, oid_registry()) {
         Ok(s) => s.to_owned(),
         _ => format!("{}", oid),
+    }
+}
+
+fn generalname_to_string(gn: &GeneralName) -> String {
+    match gn {
+        GeneralName::DNSName(name) => format!("DNSName:{}", name),
+        GeneralName::DirectoryName(n) => format!("DirName:{}", n),
+        GeneralName::EDIPartyName(obj) => format!("EDIPartyName:{:?}", obj),
+        GeneralName::IPAddress(n) => format!("IPAddress:{:?}", n),
+        GeneralName::OtherName(oid, n) => format!("OtherName:{}, {:?}", oid, n),
+        GeneralName::RFC822Name(n) => format!("RFC822Name:{}", n),
+        GeneralName::RegisteredID(oid) => format!("RegisteredID:{}", oid),
+        GeneralName::URI(n) => format!("URI:{}", n),
+        GeneralName::X400Address(obj) => format!("X400Address:{:?}", obj),
     }
 }
 
@@ -28,6 +48,25 @@ fn print_x509_extension(oid: &Oid, ext: &X509Extension) {
     match ext.parsed_extension() {
         ParsedExtension::BasicConstraints(bc) => {
             println!("      X509v3 CA: {}", bc.ca);
+        }
+        ParsedExtension::CRLDistributionPoints(points) => {
+            println!("      X509v3 CRL Distribution Points:");
+            for point in points {
+                if let Some(name) = &point.distribution_point {
+                    println!("        Full Name: {:?}", name);
+                }
+                if let Some(reasons) = &point.reasons {
+                    println!("        Reasons: {}", reasons);
+                }
+                if let Some(crl_issuer) = &point.crl_issuer {
+                    print!("        CRL Issuer: ");
+                    for gn in crl_issuer {
+                        print!("{} ", generalname_to_string(gn));
+                    }
+                    println!();
+                }
+                println!();
+            }
         }
         ParsedExtension::KeyUsage(ku) => {
             println!("      X509v3 Key Usage: {}", ku);
@@ -75,7 +114,7 @@ fn print_x509_digest_algorithm(alg: &AlgorithmIdentifier, level: usize) {
     }
 }
 
-fn print_x509_info(x509: &X509Certificate) {
+fn print_x509_info(x509: &X509Certificate) -> io::Result<()> {
     println!("  Subject: {}", x509.subject());
     println!("  Signature Algorithm:");
     print_x509_digest_algorithm(&x509.signature_algorithm, 4);
@@ -86,28 +125,66 @@ fn print_x509_info(x509: &X509Certificate) {
     println!("    NotAfter:  {}", x509.validity().not_after.to_rfc2822());
     println!("    is_valid:  {}", x509.validity().is_valid());
     println!("  Extensions:");
-    for (oid, ext) in x509.extensions() {
-        print_x509_extension(oid, ext);
+    for ext in x509.extensions() {
+        print_x509_extension(&ext.oid, ext);
     }
     println!();
+    #[cfg(feature = "validate")]
+    {
+        // structure validation status
+        let (ok, warnings, errors) = x509.validate_to_vec();
+        print!("Structure validation status: ");
+        if ok {
+            println!("Ok");
+        } else {
+            println!("FAIL");
+        }
+        for warning in &warnings {
+            println!("  [W] {}", warning);
+        }
+        for error in &errors {
+            println!("  [E] {}", error);
+        }
+        println!();
+        if VALIDATE_ERRORS_FATAL && !errors.is_empty() {
+            return Err(io::Error::new(io::ErrorKind::Other, "validation failed"));
+        }
+    }
+    Ok(())
+}
+
+fn handle_certificate(file_name: &str, data: &[u8]) -> io::Result<()> {
+    match parse_x509_certificate(data) {
+        Ok((_, x509)) => {
+            print_x509_info(&x509)?;
+            Ok(())
+        }
+        Err(e) => {
+            let s = format!("Error while parsing {}: {}", file_name, e);
+            if PARSE_ERRORS_FATAL {
+                Err(io::Error::new(io::ErrorKind::Other, s))
+            } else {
+                eprintln!("{}", s);
+                Ok(())
+            }
+        }
+    }
 }
 
 pub fn main() -> io::Result<()> {
     for file_name in env::args().skip(1) {
         println!("File: {}", file_name);
         let data = std::fs::read(file_name.clone()).expect("Unable to read file");
-        if (data[0], data[1]) == (0x30, 0x82) {
+        if matches!((data[0], data[1]), (0x30, 0x81..=0x83)) {
             // probably DER
-            let (_, x509) = parse_x509_certificate(&data).expect("Could not decode DER data");
-            print_x509_info(&x509);
+            handle_certificate(&file_name, &data)?;
         } else {
             // try as PEM
             for (n, pem) in Pem::iter_from_buffer(&data).enumerate() {
                 let pem = pem.expect("Could not decode the PEM file");
                 let data = &pem.contents;
-                let (_, x509) = parse_x509_certificate(&data).expect("Could not decode DER data");
                 println!("Certificate [{}]", n);
-                print_x509_info(&x509);
+                handle_certificate(&file_name, data)?;
             }
         }
     }
